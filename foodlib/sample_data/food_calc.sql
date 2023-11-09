fda3f30fb8f4ec15f20fc3807dcc44178eab28f9
isiko <mail@isiko404.dev>
2023-11-09 18:46:09 +0100

    Hardcode currency locale

--- foodlib/sample_data/food_calc.sql
+++ foodlib/sample_data/food_calc.sql
@@ -5,6 +5,7 @@
 -- Dumped from database version 15.4
 -- Dumped by pg_dump version 15.4
 
+SET lc_monetary to "de_DE.utf8";
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
