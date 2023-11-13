fd5ebf8793a0584fb459906722de3c28ded591e3
Dennis Kobert <dennis@kobert.dev>
2023-11-13 17:31:17 +0100

    Implement adding of ingredients

--- backend/src/frontend/ingredients_tab.rs
+++ backend/src/frontend/ingredients_tab.rs
@@ -6,6 +6,8 @@ use crate::MyAppState;
 pub(crate) fn ingredients_router() -> axum::Router<MyAppState> {
     axum::Router::new()
         .route("/search", axum::routing::post(search))
+        .route("/edit", axum::routing::put(add_ingredient))
+        .route("/add", axum::routing::get(edit_ingredient_form))
         .route("/", axum::routing::get(ingredients_view))
 }
 
@@ -28,6 +30,30 @@ pub async fn search(State(state): State<MyAppState>, query: String) -> Markup {
     }
 }
 
+pub async fn add_ingredient(
+    State(state): State<MyAppState>,
+    form: axum::extract::Form<foodlib::Ingredient>,
+) -> Markup {
+    let ingredient = form.0;
+    let Ok(ingredient_id) = state
+        .db_connection
+        .add_ingredient(ingredient.name, ingredient.energy, ingredient.comment)
+        .await
+    else {
+        return html! {
+            div id="error" class="flex flex-col items-center justify-center text-red-500" {
+                div {
+                    h1 { "Error" }
+                    p { "Failed to add ingredient" }
+                    button class="btn btn-primary" hx-get="/ingredients" hx-target="#content"  { "Back" }
+                }
+            }
+        };
+    };
+
+    ingredients_view(State(state)).await
+}
+
 pub async fn ingredients_view(State(state): State<MyAppState>) -> Markup {
     let ingredients = state
         .db_connection
@@ -36,13 +62,16 @@ pub async fn ingredients_view(State(state): State<MyAppState>) -> Markup {
         .unwrap_or_default();
 
     html! {
-        div class="flex flex-col items-center justify-center" {
+        div id="ingredients" class="flex flex-col items-center justify-center" {
             div {
                 h1 { "Ingredients" }
                 input type="search" placeholder="Search for Ingredient" id="search" name="search" autocomplete="off"
                     autofocus="autofocus" hx-post="/ingredients/search" hx-trigger="keyup changed delay:20ms, search"
                     hx-target="#search-results" hx-indicator=".htmx-indicator";
                 span class="htmx-indicator" { "Searching..." }
+                div hx-target="this"  hx-swap="outerHTML" {
+                    button hx-get="/ingredients/add" class="btn btn-primary"  { "+" }
+                }
                 table class="text-inherit table-auto object-center" {
                     thead { tr { th { "Name" } th { "Energy" } th { "Comment" } } }
                     tbody id="search-results" {
@@ -52,6 +81,23 @@ pub async fn ingredients_view(State(state): State<MyAppState>) -> Markup {
                     }
                 }
                 // Add Ingredient button
+            }
+        }
+    }
+}
+
+pub async fn edit_ingredient_form(State(state): State<MyAppState>) -> Markup {
+    html! {
+        form hx-put="/ingredients/edit" hx-target="#ingredients" hx-swap="outerHTML" {
+            div class="flex flex-col items-center justify-center" {
+                div {
+                    h1 { "Edit Ingredient" }
+                    input type="text" name="name" placeholder="Name" value="" required="required";
+                    input type="number" name="energy" placeholder="Energy" value="0" required="required";
+                    input type="text" name="comment" placeholder="Comment" value="";
+                    input type="hidden" name="ingredient_id" value="-1";
+                    button class="btn btn-primary" type="submit" { "Submit" }
+                }
             }
         }
     }
