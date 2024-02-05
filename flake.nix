edee74c1c42b23f04b5c130e3aa5b0e4fc41be44
Dennis Kobert <dennis@kobert.dev>
2024-02-05 12:25:18 +0100

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -18,63 +18,32 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        toolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ "wasm32-unknown-unknown" ];
-        };
-        rustc-wasm = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ "wasm32-unknown-unknown" ];
-          # wasm-pack needs this
-          extensions = [ "rust-src" ];
-        };
-      in with pkgs; {
+        buildInputs = with pkgs; [
+            openssl
+            expat
+            fontconfig
+            freetype
+
+            libGL
+            vulkan-loader
+            wayland
+            wayland-protocols
+            libxkbcommon
+        ];        
+        in with pkgs; {
         devShells.default = mkShell {
           packages = [
             pkgs.bacon
           ];
-          buildInputs = with pkgs; [
-            rustc-wasm
+          nativeBuildInputs = with pkgs; [
+            rustc
             nodejs
             cargo
-            cargo-watch
-            wasm-pack
-            clang
-            tailwindcss
-
-            openssl
-            openssl.dev
-            glib
-            gtk3
-            libsoup
-            webkitgtk
-            freetype
-            freetype.dev
-
             pkg-config
-
-            # Use Mold as a Linke
-            mold
-
-            # Vulkan
-            #pkgs.glslang
-            shaderc
-            vulkan-headers
-            vulkan-loader
-            #pkgs.vulkan-validation-layers
-          ];
-
-          # Hacky way to run cago through Mold
-          shellHook = ''
-          alias cargo='mold --run cargo'
-          '';
-          LD_LIBRARY_PATH = lib.makeLibraryPath [ 
-            pkgs.openssl
-            pkgs.freetype
-            pkgs.fontconfig
-            pkgs.expat
-            pkgs.libGL
-          ];
-
-          #VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+         ];
+         inherit buildInputs;
+          
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       }
     );
