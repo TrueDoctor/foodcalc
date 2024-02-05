6093544f54117f3f44cf8acdcc0d43fba52d0dc4
isiko <mail@isiko404.dev>
2024-02-05 14:22:18 +0100

    Add flake.nix (#8)
    * Migrate shell.nix to flake.nix
    
    * Refactor flake.nix
    
    ---------
    
    Co-authored-by: Dennis Kobert <dennis@kobert.dev>

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = {self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem ( system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        buildInputs = with pkgs; [
+            openssl
+            expat
+            fontconfig
+            freetype
+
+            libGL
+            vulkan-loader
+            wayland
+            wayland-protocols
+            libxkbcommon
+        ];        
+        in with pkgs; {
+        devShells.default = mkShell {
+          packages = [
+            pkgs.bacon
+          ];
+          nativeBuildInputs = with pkgs; [
+            rustc
+            nodejs
+            cargo
+            pkg-config
+         ];
+         inherit buildInputs;
+          
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
