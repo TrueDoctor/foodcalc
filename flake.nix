eff68b950786a8dc8fce6368af2e230de4252a22
isiko <mail@isiko404.dev>
2024-05-08 17:15:06 +0200

    Restructure flake.nix

--- flake.nix
+++ flake.nix
@@ -11,14 +11,59 @@
     };
   };
 
-  outputs = {self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem ( system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        buildInputs = with pkgs; [
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    let
+      overlays = [ (import rust-overlay) ];
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      #packages = forAllSystems (system:
+      #  let pkgs = import nixpkgs { inherit system overlays; };
+      #  in rec {
+      #    default = pkgs.stdenv.mkDerivation {
+      #      pname = "foodcalc";
+      #      version = "1.0.0";
+      #      src = ./cli-client; # Path to the cli-client directory
+      #      cargoSha256 = "..."; # SHA256 hash of your source code
+      #      cargoBuildFlags = [
+      #        "--release"
+      #      ];
+      #      nativeBuildInputs = [
+      #        pkgs.rustc
+      #        pkgs.cargo
+      #      ];
+      #      buildInputs = [
+      #        pkgs.openssl
+      #        pkgs.expat
+      #        pkgs.fontconfig
+      #        pkgs.freetype
+      #        pkgs.libGL
+      #        pkgs.vulkan-loader
+      #        pkgs.wayland
+      #        pkgs.wayland-protocols
+      #        pkgs.libxkbcommon
+      #      ];
+      #      outputs = [ "out" ];
+      #      installPhase = ''
+      #        mkdir -p $out/bin
+      #        cp target/release/foodctl $out/bin/
+      #      '';
+      #    };
+      #  }
+      #);
+      devShell = forAllSystems ( system:
+        let pkgs = import nixpkgs { inherit system overlays; };
+        in pkgs.mkShell {
+          packages = [
+            pkgs.bacon
+            pkgs.sqlx-cli
+          ];
+          nativeBuildInputs = with pkgs; [
+            rustc
+            nodejs
+            cargo
+            pkg-config
+         ];
+          buildInputs = with pkgs; [
             openssl
             expat
             fontconfig
@@ -34,22 +79,9 @@
             wayland
             wayland-protocols
             libxkbcommon
-        ];        
-        in with pkgs; {
-        devShells.default = mkShell {
-          packages = [
-            pkgs.bacon
           ];
-          nativeBuildInputs = with pkgs; [
-            rustc
-            nodejs
-            cargo
-            pkg-config
-         ];
-         inherit buildInputs;
-          
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        };
-      }
-    );
+        }
+      );
+    };
 }
+
