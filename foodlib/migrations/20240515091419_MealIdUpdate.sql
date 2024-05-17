440d48f8c398d1e4628652bcf954199cc11fe4c4
isiko <mail@isiko404.dev>
2024-05-17 10:10:35 +0200

    Revert "Update Meal Primary Key"
    This reverts commit 7d3a39642cb8835a45198806da376456774e7430.

--- foodlib/migrations/20240515091419_MealIdUpdate.sql
+++
@@ -1,9 +0,0 @@
-alter table public.event_meals
-    add id integer;
-
-alter table public.event_meals
-    drop constraint event_meals_pk;
-
-alter table public.event_meals
-    add constraint event_meals_pk
-        primary key (id);
