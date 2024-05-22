c1ee453471a61b3fe339940523ba62ae4fdc292c
raffiii <uounb@student.kit.edu>
2024-05-22 12:32:09 +0200

    Add single fn db export
    WIP
    
    Clean up old pdf exports
    
    Fix Cargo.lock

--- foodlib/src/typst.rs
+++ foodlib/src/typst.rs
@@ -1,6 +1,5 @@
-use std::collections::HashSet;
-
-use crate::{recipes::SubRecipe, FoodBase, RecipeStep};
+use crate::export::RecipeInfo;
+use crate::{recipes::SubRecipe, RecipeStep};
 use bigdecimal::{BigDecimal, ToPrimitive};
 use sqlx::postgres::types::PgInterval;
 use typst::{eval::Tracer, foundations::Smart};
@@ -15,43 +14,32 @@ fn create_pdf(text: String) -> eyre::Result<Vec<u8>> {
     Ok(typst_pdf::pdf(&document, Smart::Auto, None))
 }
 
-pub(crate) async fn export_recipes(
-    subrecipes: &[SubRecipe],
-    db: &FoodBase,
-) -> eyre::Result<Vec<u8>> {
-    let typst = format_recipe_typst(subrecipes, db).await?;
-    create_pdf(typst)
-}
-
 /* #recipe("Rezeptname", "22.04.2024 11:00",
   ("ingredients":("Pfeffer": 0.0111111111111, "Kartoffel": 2, "Test": 3, "test": 0.4444, ),"subrecipes":("Naan":1.0,"Dal":2,)), (
     ("title": "First", "desc":lorem(20), "duration": ("fix":10,"var":0)),
     ("title": "Second", "desc":lorem(10), "duration": ("fix":120,"var":20)),
   ))
 */
+pub async fn export_recipes(info: RecipeInfo) -> eyre::Result<Vec<u8>> {
+    let typst = format_recipe_info_typst(info).await?;
+    create_pdf(typst)
+}
 
-async fn format_recipe_typst(subrecipes: &[SubRecipe], db: &FoodBase) -> eyre::Result<String> {
+async fn format_recipe_info_typst(info: RecipeInfo) -> eyre::Result<String> {
     let mut preamble = include_str!("../templates/recipe.typ").to_string();
-    let keys = subrecipes
-        .iter()
-        .map(|sr| sr.subrecipe_id)
-        .collect::<HashSet<i32>>();
-
-    for subrecipe_id in keys {
-        let ingredients: Vec<_> = subrecipes
-            .iter()
-            .filter(|sr| sr.subrecipe_id == subrecipe_id)
-            .collect();
-        let steps = db.get_recipe_steps(subrecipe_id).await.unwrap_or_default();
-        format_subrecipe(&mut preamble, &ingredients, &steps)?;
+    for recipe in info.subrecipes {
+        let ingredients = recipe.0;
+        let steps = recipe.1;
+        format_subrecipe(&mut preamble, &ingredients[..], &steps[..], &info.date)?;
     }
     Ok(preamble)
 }
 
 fn format_subrecipe(
     text: &mut String,
-    subrecipes: &[&SubRecipe],
+    subrecipes: &[SubRecipe],
     steps: &[RecipeStep],
+    date: &String,
 ) -> eyre::Result<()> {
     let title = escape_underscore(
         &subrecipes
@@ -70,7 +58,7 @@ fn format_subrecipe(
         s.replace('_', " ")
     }
     use std::fmt::Write;
-    writeln!(text, "#recipe(\"{title}\",\"22.04.2024 11:00\",")?;
+    writeln!(text, "#recipe(\"{title}\",\"{date}\",")?;
     writeln!(text, "(\"subrecipes\":( ")?;
     for ingredient in meta_ingredients {
         writeln!(
