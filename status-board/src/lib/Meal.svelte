4e1d0de019be20e447cbca27418656015e1a8429
Dennis Kobert <dennis@kobert.dev>
2024-06-12 17:11:35 +0200

    Fix Meals not starting

--- status-board/src/lib/Meal.svelte
+++ status-board/src/lib/Meal.svelte
@@ -17,6 +17,7 @@
   import { page } from '$app/stores'
   const adminPassword = $page.url.searchParams.get('admin')
   let isAdmin =  adminPassword == "TEST";
+  let new_label = 0;
 
   function updateMeal() {
     fetch(`https://essen.campus-kit.de/api/${meal.meal_id}`, {
@@ -29,15 +30,25 @@
   }
 
   function setETA(minutes){
-    meal.eta = minutes;
+    new_label = minutes;
+
+    meal.eta = Math.ceil(minutes * 60 + Date.now() / 1000);
+    updateMeal()
+  }
+  
+  function endMeal(){
+
+    meal.end = Math.ceil(Date.now() / 1000);
     updateMeal()
   }
 
-  let absolute_eta = (meal.last_modified + meal.eta * 60)*1000;
+  let absolute_eta = meal.eta;
   let start_date = new Date(meal.start*1000);
   let end_date = new Date(meal.end*1000);
   let min_til_food = (meal.start - Date.now()/1000)/60;
+  let current_time = Date.now()/1000;
   setInterval(() => min_til_food = (meal.start - Date.now()/1000)/60, 1)
+  setInterval(() => current_time = Date.now()/1000, 1)
   console.log(meal.end)
   console.log(meal);
 </script>
@@ -45,29 +56,29 @@
   <p> {meal.recipe} {#if isAdmin} ({meal.meal_id}) {/if} ({meal.place}) </p>
   {#if !isAdmin}
     <p> Status: 
-      {#if meal.start < Date.now()}
+      {#if meal.start > Date.now() / 1000}
         🕒 Upcoming 
-        {#if min_til_food < 5}
+        {#if min_til_food < 5 && min_til_food > 0}
           (Starting in {Math.floor(min_til_food)} min {Math.floor((min_til_food % 1) * 60)} sec )
-        {:else if min_til_food < 60}
+        {:else if min_til_food < 60 && min_til_food > 0}
           (Starting in {Math.floor(min_til_food)} min )
         {:else}
           (Starting at {start_date.toLocaleString('de-DE', optionsTime)})
         {/if}
-      {:else if meal.eta == 0 || (absolute_eta < Date.now() && meal.eta >= 0)}
+      {:else if absolute_eta <= Date.now() / 1000 }
         ✅ Serving 
-      {:else if meal.eta > 0} 
-        <p>⚠️ More is on the way, comming in about {Math.floor(absolute_eta/60)} min {Math.floor((absolute_eta/60 % 1) * 60)} sec </p>
+      {:else if absolute_eta > Date.now() / 1000  && meal.eta >= 0} 
+        <p>⚠️ More is on the way, comming in about {Math.floor((absolute_eta - current_time) /60)} min {Math.floor(((absolute_eta - current_time)/60 % 1) * 60)} sec </p>
       {:else if meal.eta < 0} 
         ❌ Sorry, this meal has finished serving :/
       {/if}
     </p>
   {:else}
-    <p> ETA: <p> <input type="number" bind:value={meal.eta}>
+    <p> ETA: <p> <input type="number" bind:value={new_label}>
       {#each eta_update_variants as label}
         <button on:click={() => {setETA(label)}}>Set ETA to {label}min </button>
       {/each}
-      <button on:click={() => {setETA(-1)}}>Meal is over</button>
+      <button on:click={() => {endMeal()}}>Meal is over</button>
       <button on:click={() => {setETA(0)}}>Now Serving</button>
     <p> Custom Message:  
       <input type="text" bind:value={meal.msg}>
