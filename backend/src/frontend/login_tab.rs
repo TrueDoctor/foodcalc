560d05be68dcbff863a572416db982d776b076c1
dennis@kobert.dev <dennis@kobert.dev>
2024-07-24 18:11:15 +0200

    Show error on invalid credentials

--- backend/src/frontend/login_tab.rs
+++ backend/src/frontend/login_tab.rs
@@ -6,7 +6,7 @@ use axum::{
     Form,
 };
 use foodlib::{AuthContext, Credenitals};
-use maud::html;
+use maud::{html, Markup};
 use serde::Deserialize;
 
 use crate::MyAppState;
@@ -24,10 +24,11 @@ pub struct RedirectUrl {
 
 pub async fn login_view(Form(redirect): Form<RedirectUrl>) -> impl IntoResponse {
     let html = html! {
-        div id="login-dialog" {
+        div id="login-dialog"  {
             dialog class="dialog" open="open" id="login-dialog" {
                 div class="flex items-center justify-center" {
-                    form method="post" action=(format!("/auth/login?protected={}", redirect.protected.unwrap_or("/".to_string()))) class="flex flex-col gap-1 justify-items-center justify-center h-full w-full" {
+                    form hx-post=(format!("/auth/login?protected={}", redirect.protected.unwrap_or("/".to_string()))) class="flex flex-col gap-1 justify-items-center justify-center h-full w-full" {
+                        (wrong_credentials(true))
                         input class="text" type="text" name="username" placeholder="Username" id="username" {}
                         input class="text" type="password" placeholder="Password" name="password" id="password" {}
                         input class="btn btn-success" hx-swap="delete" hx-target="#login-dialog" type="submit" value="Login" {}
@@ -53,19 +54,36 @@ pub struct LoginData {
     password: String,
 }
 
+fn wrong_credentials(hidden: bool) -> Markup {
+    match hidden {
+        true => html! { span id="login-error" hidden class="text-red-700" {} },
+        false => html! { span id="login-error" class="text-red-700" { "Wrong credentials" } },
+    }
+}
+
 async fn login_handler(
     mut auth: AuthContext,
     State(state): State<MyAppState>,
     Form(data): Form<LoginData>,
-) -> Result<impl IntoResponse, StatusCode> {
+) -> impl IntoResponse {
     let (username, password) = (data.username, data.password);
-    let user = state
+    let Ok(user) = state
         .db_connection
         .authenticate_user(Credenitals { username, password })
         .await
-        .map_err(|_| StatusCode::UNAUTHORIZED)?;
+    else {
+        return (
+            [("HX-Reswap", "outerHTML"), ("HX-Retarget", "#login-error")],
+            wrong_credentials(false),
+        )
+            .into_response();
+    };
     auth.login(&user).await.unwrap();
-    Ok(Redirect::to(&data.protected.unwrap_or("/".to_string())).into_response())
+    (
+        [("HX-Reswap", "delete"), ("HX-Retarget", "#login-dialog")],
+        (),
+    )
+        .into_response()
 }
 
 async fn logout_handler(mut auth: AuthContext) {
